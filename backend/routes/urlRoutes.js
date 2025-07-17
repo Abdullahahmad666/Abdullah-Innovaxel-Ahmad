f41682116f3b4e9356715c338d21c2dd6c24227f
Abdullah <l201326@lhr.nu.edu.pk>
2025-07-17 16:25:18 +0500

    Add Url Routes in urlRoutes.js

---
+++ backend/routes/urlRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/urlController');
+
+router.post('/', controller.createShortUrl);
+router.get('/:code', controller.getOriginalUrl);
+router.put('/:code', controller.updateUrl);
+router.delete('/:code', controller.deleteUrl);
+router.get('/:code/stats', controller.getUrlStats);
+
+module.exports = router;
